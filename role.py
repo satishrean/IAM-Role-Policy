812c47ded09a65ffd3983cff3515cd1bc3307904
YGVPRASAD <55577916+YGVPRASAD@users.noreply.github.com>
2021-03-26 16:20:48 +0530

    Update role.py

--- role.py
+++ role.py
@@ -1,5 +1,14 @@
 import boto3
 
+client = boto3.client('sts')
+service_client = boto3.client(
+        resource_type, region_name=region,
+        aws_access_key_id=response['Credentials']['AccessKeyId'],
+        aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+        aws_session_token=response['Credentials']['SessionToken']
+        )
+return service_client
+
 role_name = "assume_role"
 
 aws_con=boto3.session.Session(profile_name="default")
