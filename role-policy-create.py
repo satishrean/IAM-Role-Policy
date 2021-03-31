480c92c1c23c71d47c902e631e1d93e6796e2675
satishrean <satish.gunda@hitachivantara.com>
2021-03-31 13:11:08 +0530

    Update role-policy-create.py

--- role-policy-create.py
+++ role-policy-create.py
@@ -58,7 +58,7 @@ for opt, arg in opts:
         for rpart in role_name.split(","):
             print(rpart)
             try:
-                f = open('PIDYAWSEXSBXAPPADMUSR.json', 'r')
+                f = open(ppart+'.json', 'r')
                 rvalue = f.read()
                 #print(rvalue)
                 assume_role_document = json.loads(json.dumps(rvalue))
